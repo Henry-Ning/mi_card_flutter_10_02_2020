ffde0099cc4a9d191d42c1900b9c3faea7ceb8ad
Henry-Ning <henryhuning@gmail.com>
2020-10-02 07:02:48 -0700

    45. How to Use Container Widgets 11min

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MyApp()
-  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -11,8 +9,19 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.white,
-        body: Container(),
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Container(
+            height: 100,
+            width: 100,
+            margin: EdgeInsets.only(left: 30,),
+            padding: EdgeInsets.all(20),
+            color: Colors.white,
+            child: Text(
+              'Hello',
+            ),
+          ),
+        ),
       ),
     );
   }
